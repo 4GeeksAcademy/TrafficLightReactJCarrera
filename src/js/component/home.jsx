3fdfc77f985dcd05e2428589e4f6c77a22f98468
JessCP07 <jjcarrera04@gmail.com>
2024-11-20 19:22:28 +0000

    Traffic-Lights

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+
+const Light = ({color, currentColor, setCurrentColor}) => {
+	const on = currentColor === color;
+
+	const setColorToCurrentColor = () => {
+		setCurrentColor(color);
+	}
+
+
+	return (
+		<div onClick={setColorToCurrentColor} className={`light ${color} ${on ? "on" : "off"}`}></div>
+	);
+}
+
+
+const TraffictLight = () => {
+	let [currentColor, setCurrentColor] = useState("green"); 
+
+	return (
+		<div className="trafficlight">
+			<Light setCurrentColor={setCurrentColor} color="red" currentColor={currentColor} />
+			<Light setCurrentColor={setCurrentColor} color="yellow" currentColor={currentColor} />
+			<Light setCurrentColor={setCurrentColor} color="green" currentColor={currentColor} />
+		</div>
+	); 
+
+}
+
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5"> 🤚Traffic-Lights!🛸</h1>
+			<TraffictLight />
 		</div>
 	);
 };
